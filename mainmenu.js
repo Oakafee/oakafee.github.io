b5a46e1189cc7e6afe3feb5497feb1db4bbab06f
Oakafee <jefrttt@gmail.com>
2016-11-28 12:49:04 -0500

    Add files via upload

---
+++ mainmenu.js
@@ -0,0 +1,6 @@
+function highlightItem(id) {
+	document.getElementById( id ).style.backgroundColor = "white";
+}
+function unhighlightItem (id) {
+	document.getElementById( id ).style.backgroundColor = "#4d4d4d";
+}
