0023c91db51798af7eecf022940dde5060f4990d
Oakafee <jefrttt@gmail.com>
2016-11-29 15:02:03 -0500

    Update mainmenu.js

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -1,5 +1,5 @@
 function highlightItem(id) {
-	document.getElementById( id ).style.backgroundColor = "white";
+	document.getElementById( id ).style.backgroundColor = "#6b6b6b";
 }
 function unhighlightItem (id) {
 	document.getElementById( id ).style.backgroundColor = "#4d4d4d";
